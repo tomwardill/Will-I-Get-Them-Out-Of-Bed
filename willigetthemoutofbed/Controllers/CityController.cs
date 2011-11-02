da2233550e251bad388e12a49a3b1a181782fd93
Tom Wardill <tom@howrandom.net>
2011-11-02 10:48:31 +0000

    Working, ish

---
+++ willigetthemoutofbed/Controllers/CityController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace willigetthemoutofbed.Controllers
+{
+    public class CityController : Controller
+    {
+        //
+        // GET: /City/
+
+        public ActionResult Lookup(string id)
+        {
+            ViewBag.City = id;
+
+            var availableTimeZones = TimeZoneInfo.GetSystemTimeZones();
+            Dictionary<string, TimeZoneInfo> data = new Dictionary<string, TimeZoneInfo>();
+
+            foreach (var zone in availableTimeZones)
+            {
+                var names = zone.ToString().Split(')')[1];
+                foreach (var name in names.Split(','))
+                {
+                    data[name.ToLower().Trim()] = zone;
+                }
+            }
+
+            var givenZone = data[id.ToLower()];
+
+            ViewBag.GivenZone = givenZone;
+
+            return View();
+        }
+
+    }
+}
--- willigetthemoutofbed/Controllers/HomeController.cs
+++ willigetthemoutofbed/Controllers/HomeController.cs
@@ -16,90 +16,10 @@ namespace willigetthemoutofbed.Controllers
             return View();
         }
 
-        //
-        // GET: /Home/Details/5
-
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        //
-        // GET: /Home/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        } 
-
-        //
-        // POST: /Home/Create
-
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Index(FormCollection collection)
         {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-        
-        //
-        // GET: /Home/Edit/5
- 
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Home/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
- 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Home/Delete/5
- 
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Home/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
- 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            return RedirectToAction("Lookup", "City", new { id = collection["city"] });
         }
     }
 }
